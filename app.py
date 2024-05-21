59efa674be33610469c9274e1214b230de0ab496
BC1337 <brettcomrie@gmail.com>
2024-05-21 09:04:26 -0500

    Completed creating a simple RESTful API with python and FastAPI, this was very intuative for the first time seeing it, going to check out DJANGO know, because that was a pretty good dev experience

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, HTTPException
-from typing  import Optional 
+from typing  import Optional
+from pydantic import BaseModel
 
 students = [
 	{'name': 'Student 1', 'age': 20},
 	{'name': 'Student 2', 'age': 18},
 	{'name': 'Student 3', 'age': 16}
 ]
+
+class Student(BaseModel):
+	name: str
+	age: int
 
 app = FastAPI()
 
@@ -23,10 +28,35 @@ def user_list(min: Optional[int] = None, max: Optional[int] = None):
 	return {'students': students}
 
 
+
 @app.get('/students/{student_id}')
 def user_detail(student_id: int):
 	student_check(student_id)
 	return {'student': students[student_id]}
+
+
+@app.post('/students')
+def user_add(student: Student):
+	students.append(student)
+
+	return {'student': students[-1]}
+
+
+@app.put('/students/{student_id}')
+def user_update(student: Student, student_id: int):
+	student_check(student_id)
+	students[student_id].update(student)
+
+	return {'student': students[student_id]}
+
+
+@app.delete('/students/{student_id}')
+def user_delete(student_id: int):
+	student_check(student_id)
+	del students[student_id]
+
+	return {'students': students}
+
 
 def student_check(student_id):
 	if not students[student_id]:
